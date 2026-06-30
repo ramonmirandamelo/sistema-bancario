0e1756d95f44f19ca8e8c470c13f004407418542
Ramon Miranda Melo <141959890+ramonmirandamelo@users.noreply.github.com>
2026-06-30 20:26:19 -0300

    Update sistema_bancario.py
    nova logica para calcular o limite do valor de saque diario e aviso de saque efetuado com sucesso!

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -56,6 +56,10 @@ while True:
         print("\n================ EXTRATO ================")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
+
+
+
+        
         menu = """
 [1] Depositar
 [2] Sacar
@@ -122,6 +126,93 @@ while True:
 
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
+        print("==========================================")
+
+    elif opcao == "0":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+
+
+
+
+menu = """
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+
+=> """
+
+saldo = 0
+limite = 1200
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 10
+#  NOVA VARIÁVEL PARA ACUMULAR OS SAQUES DO DIA
+valor_sacado_hoje = 0 
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+            print(f"Depósito de R$ {valor:.2f} efetuado com sucesso!")
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        # mudança de logica aqui 
+        # Agora checa se uma unica operação passa de 1200 OU se o que ele já sacou + o novo valor vai passar de 1200
+        excedeu_limite_individual = valor > limite
+        excedeu_limite_diario = (valor_sacado_hoje + valor) > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite_individual:
+            print(f"Operação falhou! O valor máximo permitido por saque individual é de R$ {limite:.2f}.")
+
+        elif excedeu_limite_diario:
+            saldo_restante_no_limite = limite - valor_sacado_hoje
+            print("Operação falhou! Esse saque excede o seu limite diário acumulado de R$ 1200.00.")
+            print(f"Você já sacou R$ {valor_sacado_hoje:.2f} hoje. Ainda pode sacar no máximo R$ {saldo_restante_no_limite:.2f}.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+            # se o saque deu certo,somamos o valor no acumulo do dia
+            valor_sacado_hoje += valor 
+            print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print(f"Saques realizados hoje: {numero_saques}/{LIMITE_SAQUES}")
+        # adicionei no estrato para o cliente conseguir ver quanto do limite ja usou
+        print(f"Total sacado hoje: R$ {valor_sacado_hoje:.2f} / R$ {limite:.2f}") 
         print("==========================================")
 
     elif opcao == "0":
